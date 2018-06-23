fa2f0ce41634a8c19424c93dd3c64e78c9306c0c
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-06-23 17:29:07 +0200

    Uploading the project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Przemysław Smokowski',
+        username: 'Smoku',
+        email: 'Przemek@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Klaudia Cieszyńska',
+        username: 'Klaudi',
+        email: 'Klaudia@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Patryk Kamelski',
+        username: 'Kamel',
+        email: 'Patryk@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Krzysztof Majewski',
+        username: 'Maja',
+        email: 'Krzysztof@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Marta Gawrońska',
+        username: 'Gawronisia',
+        email: 'Marta@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Dawid Stawicki',
+        username: 'Stawiś',
+        email: 'Dawid@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Michał Derkowski',
+        username: 'Michu',
+        email: 'Michał@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Paweł Kamelski',
+        username: 'Tuptuk',
+        email: 'Paweł@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Aron Kamelski',
+        username: 'Aronek',
+        email: 'Aron@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Zira Kamelska',
+        username: 'Zirka',
+        email: 'Zira@gmail.com'
+    }
+];
